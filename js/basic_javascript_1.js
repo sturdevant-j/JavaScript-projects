61edd5a25736d1935610661816ca3b009a7b1d4c
sturdevantj <58966417+sturdevant-j@users.noreply.github.com>
2020-03-11 21:14:20 -0700

    javascript

---
+++ js/basic_javascript_1.js
@@ -0,0 +1,20 @@
+window.alert("hello, world!");
+document.write("Lisa told Bart, \"eat my shorts\" bart responded");
+var A = "This is a string";
+var A = A.fontcolor("blue");
+document.write(A);
+var B = "hello everyone";
+window.alert (B);
+document.write("hello"+ "world");
+// concatanate and string
+var C = "hello friends" + "its good to see you again";
+var C = C.fontcolor("red");
+document.write(C);
+// multiple variable in one statement
+var student = "Kathy", teacher = "Mike", mentor = "Barbara";
+document.write(mentor.fontcolor("pink"));
+// assigns font color
+var X = "I am in a purple frame of mind";
+var X = X.fontcolor("purple");
+document.write(X);
+document.write(3+3);
